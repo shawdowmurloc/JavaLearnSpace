052f7cef581f19708b1e2c7d82e5f4ead6c43901
shawdowmurloc <m13940159318@163.com>
2021-07-18 17:02:33 +0800

    casDemo&&DeadLockDemo

---
+++ CASDemo/src/main/java/cas.java
@@ -0,0 +1,11 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+//class myData{
+//    AtomicInteger atomicInteger = new AtomicInteger(5);
+//}
+public class cas {
+    public static void main(String[] args) {
+        //创建一个原子类
+        AtomicInteger atomicInteger = new AtomicInteger(5);
+    }
+}
